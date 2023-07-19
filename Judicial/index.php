08db80d94a68d83226f08203acd0439cbdfbe683
Ian Kiprotich <90966528+Ian-Kiprotich@users.noreply.github.com>
2023-07-19 15:06:48 +0300

    Add files via upload

---
+++ Judicial/index.php
@@ -0,0 +1,120 @@
+<!--Header Code-->
+<?php include 'inc/header.php'?>
+<!--Header Code Ends Here-->
+
+<!--Body Code Starts Here-->
+<div class="showcase">
+    <button>Get Started Here</button>
+    <div class="intro">
+        <h1>Judicial System</h1>
+        <p>Welcome to Milimani Law Courts Judicial Appraisal System</p>
+    </div>
+</div>
+
+<div class="mainContainer">
+    <div class="details">
+        <div class="mainContent">
+         <p>This open portal allows you to choose a range of services based on your ajenda. Please explore the appropriate categories as given as per your court customer. For extra help contact the support staff.</p>
+        </div>
+        <div class="buttonBox">
+          <a href=""><button>Create an account here</button></a>
+        </div>
+    </div>
+
+
+    <br>
+    <br>
+    <div class="navbar" id="navbarholder">
+    <ul>
+        <li class="navitem">Land</li>
+        <li class="navitem">Kadhis</li>
+        <li class="navitem">Marital</li>
+        <li class="navitem">Family</li>
+        <li class="navitem">Military</li>
+        <li class="navitem">Supreme</li>
+    </ul>
+</div>
+
+<!--Content list of Cases-->
+
+    <div class="casebox">
+        <div class="caseHead"><p>ID: C/LAW/DD468 Delton Spectron River Diti Case</p></div>
+        <div class="caseDesc">Delton Spectron Case on oil spil and river claims around Diti Area. Major sessions are almost being completed.</div>
+        <div class="caseProgress">
+            <div class="progressbarBox"><div class="progressbar"><div class="bar"></div></div></div>
+            <div class="progress">39% Progress   By Lawyer Stephen Letoo</div>
+        </div>
+    </div>
+
+</div>
+
+
+<br>
+<br>
+
+<div class="casebox">
+                         <div class="caseHead"><p>ID: L/KADH/VD101 Ahmed and Hassan Family Feud </p></div>
+                        <div class="caseDesc">Ahmed and Hassan filed a case in the Kadhis Court and the case is still on the prelimary stages. Major sessions are not yet completed.</div>
+                        <div class="caseProgress">
+                    <div class="progressbarBox"><div class="progressbar"><div class="bar"></div></div></div>
+                        <div class="progress">27%  on Progress     By Advocate Ahmednassir Abdilahii.</div>
+        </div>
+    </div>
+
+
+    <br>
+<br>
+<div class="casebox">
+                         <div class="caseHead"><p>ID: C/MART/DD909 John and Mary Case</p></div>
+                        <div class="caseDesc">Mary and John filed a case in the Marital Court and the case is almost done. Major sessions are almost being completed.</div>
+                        <div class="caseProgress">
+                    <div class="progressbarBox"><div class="progressbar"><div class="bar"></div></div></div>
+                        <div class="progress">27%  on Progress     By Advocate Miss Gladys Shollei</div>
+        </div>
+    </div>
+
+    <br>
+    <br>
+
+
+    <div class="casebox">
+        <div class="caseHead"><p>ID: KL/FAM/S909 Tulwet and Kimani's Family Case</p></div>
+        <div class="caseDesc">Tulwet and Kimani family filed a case pn the issue of inheritance of their late grandparentd. Major sessions are being completed in this week court session.</div>
+        <div class="caseProgress">
+            <div class="progressbarBox"><div class="progressbar"><div class="bar"></div></div></div>
+            <div class="progress">100% Done       By Advocate Kiprop</div>
+        </div>
+    </div>
+
+</div>
+<br>
+    <br>
+
+           <div class="casebox">
+                <div class="caseHead"><p>ID: C/MART/01/07/DD08 Leutinant (Rtd) Richard Kipchumba case on multiple man slaughter. This is one of the major case in the Martial court and its on its prelimary stages. </p></div>
+        <div class="caseDesc">Commander Kinyanjui filed a case in the Martial Court and the case is on the prelimary stages. Major sessions are not yet done and the case is on going.</div>
+        <div class="caseProgress">
+            <div class="progressbarBox"><div class="progressbar"><div class="bar"></div></div></div>
+            <div class="progress">21% Progress By Senior Counsel Njuguna</div>
+        </div>
+    </div>
+
+</div>
+<br>
+    <br>
+
+< <div class="casebox">
+        <div class="caseHead"><p>ID: C/SUP/S007 MajiMaji Constituency Election Case</p></div>
+        <div class="caseDesc">The member of Parliament of MajiMaji filed a case on the recent election against his opponents. Major sessions are almost being completed.</div>
+        <div class="caseProgress">
+            <div class="progressbarBox"><div class="progressbar"><div class="bar"></div></div></div>
+            <div class="progress">100% Done       By Advocate Kiprop</div>
+        </div>
+    </div>
+
+</div>
+<!--Body Code Ends Here-->
+
+<!--Footer Code Starts Here-->
+<?php include 'inc/footer.php'?>
+<!--Footer Code Ends Here-->
